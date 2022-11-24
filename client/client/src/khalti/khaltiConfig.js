dc051c27af962164a1593c953dc87dfa375e8234
@bipingiri07 <bipin@supremeitsolutions.com>
2022-11-24 11:37:48 +0545

    otp khalti

--- client/client/src/khalti/khaltiConfig.js
+++ client/client/src/khalti/khaltiConfig.js
@@ -4,7 +4,7 @@ import KhaltiCheckout from 'khalti-checkout-web';
 import apiClient from '../api/index';
 let config = {
   // replace this key with yours
-  publicKey: 'test_public_key_86b647c1a4004b5db9321a90a93e9074',
+  publicKey: '',
   productIdentity: '1234567',
   productName: 'Multishop',
   productUrl: 'http://127.0.0.1:3000',
